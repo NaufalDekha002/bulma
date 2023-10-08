a09b5be01e3a7e86d3a4daa3206ec33e21ce4620
MSyahidAlFajri <masyahida963@gmail.com>
2023-10-08 09:27:36 +0700

    data gak masuk tros

--- js/tim.js
+++ js/tim.js
@@ -1,4 +1,14 @@
 import data from "./data.json" assert {type: "json"};
 import {setInner} from "https://jscroot.github.io/element/croot.js";
 
-setInner("tim", data.tim);
+setInner("tim", `
+<h1>${data.tim.nama}</h1>
+<p>${data.tim.npm}</p>
+<p>${data.tim.email}</p>
+`); 
+
+setInner("tim", `
+<h1>${data.tim.nama}</h1>
+<p>${data.tim.npm}</p>
+<p>${data.tim.email}</p>
+`); 
